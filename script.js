27f6095cba27309671653235d356317779fe3d81
Abdulkarim Alarmanazi <aa171@students.kiron.ngo>
2021-05-08 02:03:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 var bannerImage2 = document.getElementsByClassName('section-bigimg-container ')
-document.bannerImage2.style.backgroundSize = "60px 120px";
+document.bannerImage2.style.backgroundSize = "60px 120px !important";
