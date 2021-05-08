79a1622e367d8bd7e7003a664cdf441a250826b4
Abdulkarim Alarmanazi <aa171@students.kiron.ngo>
2021-05-08 02:16:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //    var bannerImage2 = document.getElementsByClassName('section-bigimg-container');
+       var bannerImage2 = document.getElementsByClassName('section-bigimg-container');
     document.getElementsByID('wrap').style.backgroundSize = "100% 100%";
     // var bannerImage2 = document.getElementsByClassName('section-bigimg-container ');
     document.bannerImage2.style.backgroundSize = "60px 120px";
